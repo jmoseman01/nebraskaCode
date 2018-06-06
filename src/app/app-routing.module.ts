09109a974eb10d400c4ffe81163bcfdad683f31b
Jesse Moseman <jmoseman01@gmail.com>
2018-06-06 15:14:06 -0500

    todo list with check and delete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
+import { TodoComponent } from './todo/todo.component';
+
+
 const routes: Routes = [
   {
     path: '',
-    children: []
+    component: TodoComponent
   },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,35 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { TodoComponent } from './todo/todo.component';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrashAlt, faSquare, faCheckSquare } from '@fortawesome/free-regular-svg-icons';
 
 
+library.add(faTrashAlt, faCheckSquare, faSquare);
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule,
     NgbModule.forRoot(),
-    FontAwesomeModule,
-    HttpClientModule,
-  ],
+    FontAwesomeModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
